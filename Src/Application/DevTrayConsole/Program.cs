4b6fa70666320b3e3c0bc4fb86c276f9ffe63ff5
Nika <tortoise.2473@gmail.com>
2025-10-20 23:15:25 +0900

    ログ出力のサンプルコードを追加

--- Src/Application/DevTrayConsole/Program.cs
+++ Src/Application/DevTrayConsole/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+// dotnet add package Microsoft.Extensions.Hosting
+
+
+// ロガーファクトリを直接作る
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddConsole()
+        .AddDebug();
+});
+
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("一度だけログを出力します");
+logger.LogWarning("一度だけログを出力します");
+logger.LogError("一度だけログを出力します");
+
+// ここでプログラム終了
+Console.WriteLine("処理終了");
+
+
+
